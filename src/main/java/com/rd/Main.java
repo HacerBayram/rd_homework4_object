2863e75c2393663a3e9a3dfe73a09f7dd99526d2
Hacer Bayram <hacerbayram95@gmail.com>
2024-05-22 23:09:50 +0300

    second try

--- src/main/java/com/rd/Main.java
+++ src/main/java/com/rd/Main.java
@@ -1,19 +1,25 @@
 package com.rd;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hellooo and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Employess employee1 = new Employess();
+        Employess employee2 = new Employess();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        employee1.ID= 1;
+        employee1.Name = "Hacer";
+        employee1.Surname = "Bayram";
+        employee1.Age=29;
+        employee1.Department= "IT";
+
+        employee2.ID =2;
+        employee2.Name ="Selin";
+        employee2.Surname = "Tektas";
+        employee2.Age=25;
+        employee2.Department= "Finance";
+
+        employee1.added();
+        employee2.added();
+
         }
     }
-}
